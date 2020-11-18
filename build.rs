e0b146bb13c643c099883e6389f013ae13a969ea
Andrii Yurchuk <ay@mntw.re>
2020-11-18 16:22:26 +0100

    Add a check for outdated completions

--- build.rs
+++ build.rs
@@ -1,10 +1,12 @@
 use clap_generate::generate_to;
 use clap_generate::generators::{Bash, Fish, Zsh};
+use std::env;
 
 include!("src/cli.rs");
 
 fn main() {
-    generate_to::<Bash, _, _>(&mut build_cli(), "orthanc", "completion");
-    generate_to::<Fish, _, _>(&mut build_cli(), "orthanc", "completion");
-    generate_to::<Zsh, _, _>(&mut build_cli(), "orthanc", "completion");
+    let output_dir = env::var("ORC_COMPLETION_OUTPUT_DIR").unwrap();
+    generate_to::<Bash, _, _>(&mut build_cli(), "orthanc", &output_dir);
+    generate_to::<Fish, _, _>(&mut build_cli(), "orthanc", &output_dir);
+    generate_to::<Zsh, _, _>(&mut build_cli(), "orthanc", &output_dir);
 }
