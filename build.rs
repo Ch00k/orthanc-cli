5afd50a4e66e4b15a180c4172171ab25e6bab750
Andrii Yurchuk <ay@mntw.re>
2020-11-23 11:00:55 +0100

    Fix completion test

--- build.rs
+++ build.rs
@@ -5,7 +5,8 @@ use std::env;
 include!("src/cli.rs");
 
 fn main() {
-    let output_dir = env::var("ORC_COMPLETION_OUTPUT_DIR").unwrap();
+    let output_dir =
+        env::var("ORC_COMPLETION_OUTPUT_DIR").unwrap_or("/tmp/orc_completion".to_string());
     generate_to::<Bash, _, _>(&mut build_cli(), "orthanc", &output_dir);
     generate_to::<Fish, _, _>(&mut build_cli(), "orthanc", &output_dir);
     generate_to::<Zsh, _, _>(&mut build_cli(), "orthanc", &output_dir);
